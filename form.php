d66425bf701bdc440bfcde54321d75ae9dad094a
wwcoderecife <karina@womenwhocode.com>
2016-11-20 20:27:55 -0300

    update

--- form.php
+++ form.php
@@ -65,7 +65,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="index.html">BootZard - Bootstrap Wizard Template</a>
+					<a class="navbar-brand" href="index.html">Womanity Ecossistema</a>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="top-navbar-1">
@@ -100,8 +100,10 @@
                 <div class="row form_inicial">
                     <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 form-box">
                     	<form role="form" action="" method="post" class="f1">
+       
+                        <!-- Etapas Form Organizaçāo-->
 
-                    		<h3>Cadastro Womanity Ecossistema</h3>
+                    		<h3>Cadastro Womanity Ecossistema - Tipo Organizacional</h3>
                     		<p>Preencha os campos obrigatórios *</p>
                     		<div class="f1-steps">
                     			<div class="f1-progress">
@@ -109,54 +111,63 @@
                     			</div>
                     			<div class="f1-step active">
                     				<div class="f1-step-icon"><i class="fa fa-user"></i></div>
-                    				<p>Dados Pessoais 1</p>
+                    				<p>Dados Pessoais</p>
                     			</div>
                     			<div class="f1-step">
                     				<div class="f1-step-icon"><i class="fa fa-key"></i></div>
-                    				<p>Dados Pessoais 2</p>
+                    				<p>Dados Organizacionais </p>
                     			</div>
                     		    <div class="f1-step">
                     				<div class="f1-step-icon"><i class="fa fa-twitter"></i></div>
-                    				<p>Dados Organizacionais 1</p>
+                    				<p>Enviar</p>
                     			</div>
+                                <!--
                                 <div class="f1-step">
                                     <div class="f1-step-icon"><i class="fa fa-question"></i></div>
                                     <p>Dados Organizacionais 2</p>
                                 </div>
+                            -->
 
                     		</div>
+
+
+                            <!-- Formulário: Tipo Organizaçāo -->
                     		
-                    		<fieldset>
+                            <?php alert($tipo);?>
+
+                    		  <fieldset>
                     		    <h4>Dados Pessoais:</h4>
                     			<div class="form-group">
                     			    <label class="sr-only" for="f1-first-name">Nome</label>
-                                    <input type="text" name="f1-first-name" placeholder="Nome" class="f1-first-name form-control" id="f1-first-name" required data-error = "Campo está em branco, favor preencher.">
+                                    <input type="text" name="f1-first-name" placeholder="Nome do responsável pelo preenchimento do cadastro" class="f1-first-name form-control" id="f1-first-name" required data-error = "Campo está em branco, favor preencher.">
                                     <div class="help-block with-errors"></div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="sr-only" for="f1-last-name">Cargo</label>
-                                    <input type="text" name="f1-last-name" placeholder="Cargo" class="f1-last-name form-control" id="f1-last-name">
+                                    <label class="sr-only" for="f1-cargo1">Cargo</label>
+                                    <input type="text" name="f1-last-name" placeholder="Cargo" class="f1-last-name form-control" id="cargo">
                                 </div>
+
+                                <!--
 
                                 <div class="form-group">
                                     <label class="sr-only" for="f1-last-name">RG</label>
                                     <input type="text" name="f1-last-name" placeholder="RG" class="f1-last-name form-control" id="f1-last-name">
                                 </div>
+                            -->
+                            
                                 <div class="form-group">
-                                    <label class="sr-only" for="f1-last-name">E-mail</label>
-                                    <input type="text" name="f1-last-name" placeholder="E-mail" class="f1-last-name form-control" id="f1-last-name">
+                                    <label class="sr-only" for="f1-email1">E-mail</label>
+                                    <input type="text" name="f1-last-name" placeholder="E-mail" class="f1-last-name form-control" id="f1-email1">
                                     
                                 </div>
 
-                                <div class="form-group">
-                                    <label class="sr-only" for="f1-last-name">Confirma E-mail</label>
-                                    <input type="text" name="f1-last-name" placeholder="Confirma E-mail" class="f1-last-name form-control" id="f1-last-name">
-                                </div>
 
                                 <div class="form-group">
-                                    <label class="sr-only" for="f1-last-name">Senha</label>
-                                    <input type="text" name="f1-last-name" placeholder="Senha" class="f1-last-name form-control" id="f1-last-name">
+                                    <label class="sr-only" for="f1-confirma_email">Confirma E-mail</label>
+                                    <input type="text" name="f1-last-name" placeholder="Confirma E-mail" class="f1-last-name form-control" id="f1-email1">
                                 </div>
+
+                                
                                 <div class="form-group">
                                     <label class="sr-only" for="f1-last-name">Celular</label>
                                     <input type="text" name="f1-last-name" placeholder="Celular" class="f1-last-name form-control" id="f1-last-name">
@@ -168,13 +179,45 @@
                                     	                 class="f1-about-yourself form-control" id="f1-about-yourself"></textarea>
                                 </div>
                             -->
+
+                            <div class="form-group">
+                                    <label class="sr-only" for="f1-first-name">Nome</label>
+                                    <input type="text" name="f1-first-name" placeholder="Nome do responsável pela organização ou suplente" class="f1-first-name form-control" id="f1-first-name" required data-error = "Campo está em branco, favor preencher.">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="sr-only" for="f1-last-name">Cargo</label>
+                                    <input type="text" name="f1-last-name" placeholder="Cargo" class="f1-last-name form-control" id="f1-last-name">
+                                </div>
+
+
+                            <div class="form-group">
+                                    <label class="sr-only" for="f1-last-name">E-mail</label>
+                                    <input type="text" name="f1-last-name" placeholder="E-mail" class="f1-last-name form-control" id="f1-last-name">
+                                    
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label class="sr-only" for="f1-last-name">Confirma E-mail</label>
+                                    <input type="text" name="f1-last-name" placeholder="Confirma E-mail" class="f1-last-name form-control" id="f1-last-name">
+                                </div>
+
+                                
+                                <div class="form-group">
+                                    <label class="sr-only" for="f1-last-name">Celular</label>
+                                    <input type="text" name="f1-last-name" placeholder="Celular" class="f1-last-name form-control" id="f1-last-name">
+                                </div>
+
                                 <div class="f1-buttons">
                                     <button type="button" class="btn btn-next">Next</button>
                                 </div>
                             </fieldset>
 
+                                 <!--Etapa 2 Formulário: Organizaçāo -->
+
                             <fieldset>
-                                <h4>Set up your account:</h4>
+                                <h4>Dados Organizacionais:</h4>
                                 <div class="form-group">
                                     <label class="sr-only" for="f1-email">Email</label>
                                     <input type="text" name="f1-email" placeholder="Email..." class="f1-email form-control" id="f1-email">
