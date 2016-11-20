c208686dbb548a21969792957b3cab8c1b1a60a4
wwcoderecife <karina@womenwhocode.com>
2016-11-20 20:31:57 -0300

    update

--- index.php
+++ index.php
@@ -94,7 +94,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="index.html">BootZard - Bootstrap Wizard Template</a>
+					<a class="navbar-brand" href="index.html">Womanity Ecossistema</a>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="top-navbar-1">
