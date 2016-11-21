179a92aadc3bbbfb3aafdd82b0cedfb070725d3a
willany <willany_lp@hotmail.com>
2016-11-21 01:19:07 -0300

    Form inciativa

--- form_iniciativa.php
+++ form_iniciativa.php
@@ -7,73 +7,29 @@
     		<p>Preencha os campos obrigatórios *</p>
     		<div class="f1-steps">
     			<div class="f1-progress">
-    			    <div class="f1-progress-line" data-now-value="12.5" data-number-of-steps="2" style="width: 12.5%;"></div>
+    			    <div class="f1-progress-line" data-now-value="8" data-number-of-steps="5" style="width: 8%;"></div>
     			</div>
-    			<div class="f1-step">
+    			<div class="f1-step step-5 active">
     				<div class="f1-step-icon"><i class="fa fa-address-card-o"></i></div>
     				<p>Dados Pessoais</p>
     			</div>
-    			<div class="f1-step active">
+    			<div class="f1-step step-5">
     				<div class="f1-step-icon"><i class="fa fa-user"></i></div>
     				<p>Iniciativa</p>
-    			</div>
+    			</div>   
+                <div class="f1-step step-5">
+                    <div class="f1-step-icon"><i class="fa fa-map-marker"></i></div>
+                    <p>Endereço</p>
+                </div>
+                <div class="f1-step step-5">
+                    <div class="f1-step-icon"><i class="fa fa-desktop"></i></div>
+                    <p>Redes Sociais</p>
+                </div>
+                <div class="f1-step step-5">
+                    <div class="f1-step-icon"><i class="fa fa-hand-o-right"></i></div>
+                    <p>Indicação</p>
+                </div>              
     		</div>
-    		
-    		
-
-            <fieldset>
-                <h5>A sua iniciativa está dentro de uma organização juridicamente constituída?*</h5>
-                <div class="form-group">                                   
-                    <label class="radio-inline">
-                        <input type="radio" name="iniciativa-ong" value="sim" class="form-control-radio"> Sim
-                    </label>
-
-                    <label class="radio-inline">
-                        <input type="radio" name="iniciativa-ong" value="nao" class="form-control-radio"> Não
-                    </label>
-                </div>
-
-                <h5>A sua iniciativa está localizada na…*</h5>
-                <div class="form-group">
-                   <label class="radio-inline">
-                        <input type="radio" name="iniciativa-zona" value="rural" class="form-control-radio"> Zona Rural
-                    </label>
-
-                    <label class="radio-inline">
-                        <input type="radio" name="iniciativa-zona" value="urbana" class="form-control-radio"> Zona Urbana
-                    </label>
-                </div>
-
-                <h5>Nome da Iniciativa*</h5>
-                <div class="form-group">
-                    <label class="sr-only" for="f1-repeat-password">Nome</label>
-                    <input type="text" name="iniciativa-nome" maxlength="50">
-                </div>
-
-
-                <h5>Sigla da iniciativa:</h5>
-                <div class="form-group">
-                    <label class="sr-only" for="f1-repeat-password">Sigla</label>
-                    <input type="text" name="iniciativa-sigla" style="text-transform:uppercase">
-                </div>
-
-                <h5>Telefone da iniciativa*</h5>
-                <div class="form-group">
-                    <label class="sr-only" for="f1-repeat-password">Telefone</label>
-                    <input type="text" name="iniciativa-telefone" placeholder="(xx) xxxxx-xxxx">
-                </div>
-
-                <h5>E-mail da iniciativa*</h5>
-                <div class="form-group">
-                    <label class="sr-only" for="f1-repeat-password">Email</label>
-                    <input type="text" name="iniciativa-email" placeholder="(xx) xxxxx-xxxx">
-                </div>
-
-                <div class="f1-buttons">
-                    <button type="button" class="btn btn-previous">Previous</button>
-                    <button type="button" class="btn btn-next">Next</button>
-                </div>
-            </fieldset>
 
             <fieldset>
                 <h4>Dados Pessoais Contato 1:</h4>
@@ -126,25 +82,202 @@
             </fieldset>
 
             <fieldset>
-                <h4>Social media profiles:</h4>
+                <h5>A sua iniciativa está dentro de uma organização juridicamente constituída?*</h5>
+                <div class="form-group">                                   
+                    <label class="radio-inline">
+                        <input type="radio" name="iniciativa-ong" value="sim" class="form-control-radio"> Sim
+                    </label>
+
+                    <label class="radio-inline">
+                        <input type="radio" name="iniciativa-ong" value="nao" class="form-control-radio"> Não
+                    </label>
+                </div>
+
+                <h5>A sua iniciativa está localizada na…*</h5>
                 <div class="form-group">
-                    <label class="sr-only" for="f1-facebook">Facebook</label>
-                    <input type="text" name="f1-facebook" placeholder="Facebook..." class="f1-facebook form-control" id="f1-facebook">
+                   <label class="radio-inline">
+                        <input type="radio" name="iniciativa-zona" value="rural" class="form-control-radio"> Zona Rural
+                    </label>
+
+                    <label class="radio-inline">
+                        <input type="radio" name="iniciativa-zona" value="urbana" class="form-control-radio"> Zona Urbana
+                    </label>
+                </div>
+
+                <h5>Nome da Iniciativa*</h5>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-repeat-password">Nome</label>
+                    <input type="text" name="iniciativa-nome" maxlength="50">
+                </div>
+
+
+                <h5>Sigla da iniciativa:</h5>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-repeat-password">Sigla</label>
+                    <input type="text" name="iniciativa-sigla" style="text-transform:uppercase">
+                </div>
+
+                <h5>Telefone da iniciativa*</h5>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-repeat-password">Telefone</label>
+                    <input type="text" name="iniciativa-telefone" placeholder="(xx) xxxxx-xxxx">
+                </div>
+
+                <h5>E-mail da iniciativa*</h5>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-repeat-password">Email</label>
+                    <input type="text" name="iniciativa-email">
+                </div>
+
+                <div class="f1-buttons">
+                    <button type="button" class="btn btn-previous">Previous</button>
+                    <button type="button" class="btn btn-next">Next</button>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-google-plus">Região</label>
+                    <select id="regiao">
+                        <option value="norte">Norte</option>
+                        <option value="nordeste">Nordeste</option>
+                        <option value="centro-oeste">Centro-Oeste</option>
+                        <option value="sul">Sul</option>
+                        <option value="sudeste">Sudeste</option>
+                    </select> *
+                </div>
+                 <div class="form-group">
+                    <label class="sr-only" for="f1-google-plus">Estado</label>
+                    <select id="estados">
+                        <option value=""></option>
+                    </select> *
                 </div>
                 <div class="form-group">
-                    <label class="sr-only" for="f1-twitter">Twitter</label>
-                    <input type="text" name="f1-twitter" placeholder="Twitter..." class="f1-twitter form-control" id="f1-twitter">
+                    <label class="sr-only" for="f1-google-plus">Cidade</label>
+                    <select id="cidades"> 
+                        <option value=""></option>
+                    </select> *
                 </div>
                 <div class="form-group">
-                    <label class="sr-only" for="f1-google-plus">Google plus</label>
-                    <input type="text" name="f1-google-plus" placeholder="Google plus..." class="f1-google-plus form-control" id="f1-google-plus">
+                    <label class="sr-only" for="f1-google-plus">Bairro</label>
+                    <input type="text" name="iniciativa-bairro" placeholder="Bairro...">
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-facebook">Rua</label>
+                    <input type="text" name="iniciativa-rua" placeholder="Rua/Av...*">
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-twitter">Numero</label>
+                    <input type="text" name="iniciativa-numero" placeholder="Numero...*">
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-google-plus">Complemento</label>
+                    <input type="text" name="iniciativa-complemento" placeholder="Complemento...*">
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-google-plus">Cep</label>
+                    <input type="text" name="iniciativa-cep" placeholder="CEP...*">
+                </div>              
+                <div class="f1-buttons">
+                    <button type="button" class="btn btn-previous">Previous</button>
+                    <button type="button" class="btn btn-next">Next</button
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <div class="form-group">
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-mouse-pointer"></i></span>
+                      <input class="form-control" type="text" placeholder="Site">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-facebook-official"></i></span>
+                      <input class="form-control" type="text" placeholder="Facebook">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group margin-bottom-sm">
+                      <span class="input-group-addon"><i class="fa fa-twitter-square"></i></span>
+                      <input class="form-control" type="text" placeholder="Twitter">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group margin-bottom-sm">
+                      <span class="input-group-addon"><i class="fa fa-instagram"></i></span>
+                      <input class="form-control" type="text" placeholder="Instagram">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group margin-bottom-sm">
+                      <span class="input-group-addon"><i class="fa fa-linkedin-square"></i></span>
+                      <input class="form-control" type="text" placeholder="Linkedin">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group margin-bottom-sm">
+                      <span class="input-group-addon"><i class="fa fa-ellipsis-h"></i></span>
+                      <input class="form-control" type="text" placeholder="Outros">
+                    </div>
+                </div>
+                <div class="f1-buttons">
+                    <button type="button" class="btn btn-previous">Previous</button>
+                    <button type="button" class="btn btn-next">Next</button>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <h4>Indique até três iniciativas que na sua opinião devem participar do processo de mapeamento:</h4>
+                <h5>Iniciativa 1</h5>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-last-name">Nome</label>
+                    <input type="text" name="f1-last-name" placeholder="Nome" class="f1-last-name form-control" id="f1-last-name">
+                </div>
+
+                <div class="form-group">
+                    <label class="sr-only" for="f1-last-name">E-mail</label>
+                    <input type="text" name="f1-last-name" placeholder="E-mail" class="f1-last-name form-control" id="f1-last-name">
+                    
+                </div>
+
+                <div class="form-group">
+                    <label class="sr-only" for="f1-last-name">Telefone</label>
+                    <input type="text" name="f1-last-name" placeholder="Telefone" class="f1-last-name form-control" id="f1-last-name">
+                </div>
+                <h5>Iniciativa 2</h5>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-last-name">Nome</label>
+                    <input type="text" name="f1-last-name" placeholder="Nome" class="f1-last-name form-control" id="f1-last-name">
+                </div>
+
+                <div class="form-group">
+                    <label class="sr-only" for="f1-last-name">E-mail</label>
+                    <input type="text" name="f1-last-name" placeholder="E-mail" class="f1-last-name form-control" id="f1-last-name">                    
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-last-name">Telefone</label>
+                    <input type="text" name="f1-last-name" placeholder="Telefone" class="f1-last-name form-control" id="f1-last-name">
+                </div>
+                <h5>Iniciativa 3</h5>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-last-name">Nome</label>
+                    <input type="text" name="f1-last-name" placeholder="Nome" class="f1-last-name form-control" id="f1-last-name">
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-last-name">E-mail</label>
+                    <input type="text" name="f1-last-name" placeholder="E-mail" class="f1-last-name form-control" id="f1-last-name">
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="f1-last-name">Telefone</label>
+                    <input type="text" name="f1-last-name" placeholder="Telefone" class="f1-last-name form-control" id="f1-last-name">
                 </div>
                 <div class="f1-buttons">
                     <button type="button" class="btn btn-previous">Previous</button>
                     <button type="submit" class="btn btn-submit">Submit</button>
                 </div>
             </fieldset>
-    	
+            
     	</form>
     </div>
 </div>
