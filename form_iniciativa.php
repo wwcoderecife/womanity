10ed1cf5af40714b63067d7f3e70187c366bfb8a
wwcoderecife <karina@womenwhocode.com>
2016-11-21 23:10:34 -0300

    update

--- form_iniciativa.php
+++ form_iniciativa.php
@@ -123,7 +123,7 @@
                     <input type="text" name="iniciativa-telefone" placeholder="(xx) xxxxx-xxxx"class="f1-last-name form-control">
                 </div>
 
-                <h5>E-mail da iniciativa*</h5>
+                <h5>E-mail da iniciativa</h5>
                 <div class="form-group">
                     <label class="sr-only" for="f1-repeat-password">Email</label>
                     <input type="text" name="iniciativa-email"class="f1-last-name form-control">
@@ -135,10 +135,12 @@
                 </div>
             </fieldset>
 
+             <!--Etapa 3 Formulário: Endereço/Organizaçāo -->
+
             <fieldset>
                 <h5>Regiāo</h5>
                 <div class="form-group">
-                    <label class="sr-only" for="f1-google-plus">Região</label>
+                    <label class="sr-only" for="f1-google-plus"></label>
                     <select class="f1-last-name form-control" id="regiao">
                         <option value="norte">Norte</option>
                         <option value="nordeste">Nordeste</option>
@@ -147,16 +149,68 @@
                         <option value="sudeste">Sudeste</option>
                     </select> 
                 </div>
+                <h5>Estado</h5>
                  <div class="form-group">
-                    <label class="sr-only" for="f1-google-plus">Estado</label>
+                    <label class="sr-only" for="f1-google-plus"></label>
                     <select class="f1-last-name form-control" id="estados">
-                        <option value=""></option>
+                        <option value="1">Acre</option>
+                        <option value="2">Alagoas</option>
+                        <option value="3">Amapá</option>
+                        <option value="4">Amazonas</option>
+                        <option value="5">Bahia</option>
+                        <option value="6">Ceará</option>
+                        <option value="7">Distrito Federal</option>
+                        <option value="8">Espírito Santo</option>
+                        <option value="9">Goiás</option>
+                        <option value="10">Maranhão</option>
+                        <option value="11">Mato Grosso</option>
+                        <option value="12">Mato Grosso do Sul</option>
+                        <option value="13">Minas Gerais</option>
+                        <option value="14">Pará</option>
+                        <option value="15">Paraíba</option>
+                        <option value="16">Paraná</option>
+                        <option value="17">Pernambuco</option>
+                        <option value="18">Piauí</option>
+                        <option value="19">Rio de Janeiro</option>
+                        <option value="20">Rio Grande do Norte</option>
+                        <option value="21">Rio Grande do Sul</option>
+                        <option value="22">Rondônia</option>
+                        <option value="23">Roraima</option>
+                        <option value="24">Santa Catarina</option>
+                        <option value="25">São Paulo</option>
+                        <option value="26">Sergipe</option>
+                        <option value="27">Tocantins</option>
                     </select> 
                 </div>
+                <h5>Cidade</h5>
                 <div class="form-group">
-                    <label class="sr-only" for="f1-google-plus">Cidade</label>
+                    <label class="sr-only" for="f1-google-plus"></label>
                     <select class="f1-last-name form-control" id="cidades"> 
-                        <option value=""></option>
+                        <option value="Rio Branco"></option>
+                        <option value="Maceió"></option>
+                        <option value="Macapá"></option>
+                        <option value="Manaus"></option>
+                        <option value="Salvador"></option>
+                        <option value="Fortaleza"></option>
+                        <option value="Brasília"></option>
+                        <option value="Vitória"></option>
+                        <option value="Goiânia"></option>
+                        <option value="São Luís"></option>
+                        <option value="Cuiabá"></option>
+                        <option value="Campo Grande"></option>
+                        <option value="Belo Horizonte"></option>
+                        <option value="Belém"></option>
+                        <option value="João Pessoa"></option>
+                        <option value="Curitiba"></option>
+                        <option value="Recife"></option>
+                        <option value="Teresina"></option>
+                        <option value="Natal"></option>
+                        <option value="Porto Velho"></option>
+                        <option value="Boa Vista"></option>
+                        <option value="Florianópolis"></option>
+                        <option value="São Paulo"></option>
+                        <option value="Aracaju"></option>
+                        <option value="Palmas"></option>
                     </select> 
                 </div>
                 <div class="form-group">
