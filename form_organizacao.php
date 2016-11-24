cb2d55acf50a921f1be7301285cb7bcd076ab6ce
wwcoderecife <karina@womenwhocode.com>
2016-11-23 22:22:11 -0300

    up

--- form_organizacao.php
+++ form_organizacao.php
@@ -661,6 +661,36 @@
     </div>
   </div>
 
+  <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="js/multiselect.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(function () {
+        $('#table1').multiSelect({
+            actcls: 'info', 
+            selector: 'tbody tr', 
+            except: ['tbody'], 
+            statics: ['.danger', '[data-no="1"]'], 
+            callback: function (items) {
+                $('#table2').empty().append(items.clone().removeClass('info').addClass('success'));
+            }
+        });
+    })
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
     
 
 
