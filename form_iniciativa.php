32a3b0937fa661aa9ed6b78e1f239e5a82620bf4
wwcoderecife <karina@womenwhocode.com>
2016-11-25 10:59:16 -0300

    up

--- form_iniciativa.php
+++ form_iniciativa.php
@@ -274,19 +274,19 @@
                  <h5>2014</h5>
                 <div class="form-group">
                     <label class="sr-only" for="f1-last-name">2014</label>
-                    <input type="number" name="f1-last-name" placeholder="R$ xxxxxx" class="f1-last-name form-control" id="iniciativa_orcamento2014"required data-error = "Campo está em branco, favor preencher.">
+                    <input type="number" name="f1-last-name" placeholder="R$ xxxxxx" class="f1-last-name form-control" id="iniciativa_orcamento2014"required data-error = "favor preencher, um valor correto.">
                     <div class="help-block with-errors"></div>
                 </div>
                 <h5>2015</h5>
                 <div class="form-group">
                     <label class="sr-only" for="f1-last-name">2015</label>
-                    <input type="number" name="f1-last-name" placeholder="R$ xxxxxx" class="f1-last-name form-control" id="iniciativa_orcamento2015" required data-error = "Campo está em branco, favor preencher.">
+                    <input type="number" name="f1-last-name" placeholder="R$ xxxxxx" class="f1-last-name form-control" id="iniciativa_orcamento2015" required data-error = "favor preencher, um valor correto.">
                     <div class="help-block with-errors"></div>
                 </div>
                 <h5>2016</h5>
                 <div class="form-group">
                     <label class="sr-only" for="f1-last-name">2016</label>
-                    <input type="number" name="f1-last-name" placeholder="R$ xxxxxx" class="f1-last-name form-control" id="iniciativa_orcamento2016"required data-error = "Campo está em branco, favor preencher.">
+                    <input type="number" name="f1-last-name" placeholder="R$ xxxxxx" class="f1-last-name form-control" id="iniciativa_orcamento2016"required data-error = "favor preencher, um valor correto.">
                     <div class="help-block with-errors"></div>
                 </div>
 
