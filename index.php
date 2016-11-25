84fbad84c56b6c1091b8709b9a1960c976124e69
wwcoderecife <karina@womenwhocode.com>
2016-11-25 09:52:56 -0300

    up

--- index.php
+++ index.php
@@ -206,7 +206,7 @@
                                 <div class="form-group">
                                     <div class="checkbox">
                                         <label>
-                                            <input type="checkbox" name="carta_principios" data-error="Você deve marcar este campo!" value="1" required>
+                                            <input type="checkbox" name="carta_principios" data-error="Você deve marcar este campo!" value="1" required>Marque este item.
                                             <ul>
                                                 Li e concordo com a carta <a href = "#" id ="check1" data-toggle="modal" data-target="#login-modal"> de príncipios e critérios *</a> 
                                                 <div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel" id= "login-modal">
@@ -235,7 +235,7 @@
                                 <div class="form-group">
                                     <div class="checkbox">
                                         <label>
-                                            <input type="checkbox" name="politica_privacidade" data-error="Você deve marcar este campo!"  value="1" required>
+                                            <input type="checkbox" name="politica_privacidade" data-error="Você deve marcar este campo!"  value="1" required>Marque este item.
                                             <ul>
                                                 Li e concordo com a  <a href = "#" id ="check2" data-toggle="modal" data-target="#login-modal2"> Política de Privacidade *</a> 
                                                 <div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel" id= "login-modal2">
