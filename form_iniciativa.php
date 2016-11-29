4ec05f0ab73cb53b0410115f087182bb9c40d0cf
wwcoderecife <karina@womenwhocode.com>
2016-11-29 00:44:57 -0300

    update

--- form_iniciativa.php
+++ form_iniciativa.php
@@ -24,10 +24,10 @@
 
 <div class="row form_inicial">
     <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 form-box">
-    	 <form role="form"  action="classes/prepare_cadastro.php" method="post" class="form-ong">
+    	 <form role="form" action="classes/prepare_cadastro.php" method="post" class="f1 form-ong">
 
     		<h3>Cadastro UNA Ecossistema<br>Iniciativa</h3>
-    		<p>Preencha os campos obrigatórios *</p>
+    		<p>Preencha os campos obrigatórios*</p>
     		<div class="f1-steps">
     			<div class="f1-progress">
     			    <div class="f1-progress-line" data-now-value="8" data-number-of-steps="5" style="width: 8%;"></div>
--- form_organizacao.php
+++ form_organizacao.php
@@ -45,7 +45,7 @@
 
 <div class="row form_inicial">
     <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 form-box">
-        <form role="form"  action="classes/prepare_cadastro.php" method="post" class="form-ong">
+        <form role="form" action="classes/prepare_cadastro.php" method="post" class="f1 form-ong">
 
             <h3>Cadastro UNA Ecossistema<br>Organizaçāo</h3>
             <p>Preencha os campos obrigatórios *</p>
