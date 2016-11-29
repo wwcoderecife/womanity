9a0aa22f6ffb1df84d22d59ff32170201e07e69e
wwcoderecife <karina@womenwhocode.com>
2016-11-29 15:26:58 -0300

    update style form organização pergunta 22

--- form_organizacao.php
+++ form_organizacao.php
@@ -580,15 +580,15 @@ Direito e acesso a alimentos de qualidade, em quantidade suficiente, saudáveis 
                  <h5>Qual é a sua função no ecossistema?*</h5>
 
                  <div class="form-group">
-                 <input type="checkbox" name="1" value="Advocacy"><h5>Advocacy<br></h5>
-                 <input type="checkbox" name="2" value="Atuação direta com mulheres, homens, LGBTT, etc"></h5>Atuação direta com mulheres, homens, LGBTT, etc<br></h5>
+                 <input type="checkbox" name="1" value="Advocacy">Advocacy<br>
+                 <input type="checkbox" name="2" value="Atuação direta com mulheres, homens, LGBTT, etc">Atuação direta com mulheres, homens, LGBTT, etc<br>
 
                  <input type="numeric"  name="" placeholder="número de beneficiários diretos" style="margin-left: 20px" class="f1-last-name form-control"><br>
-                 <input type="checkbox" name="3" value="Financiadores"><h5>Financiadores<br></h5>
-                 <input type="checkbox" name="4" value="Fortalecedor institucional"><h5>Fortalecedor institucional<br></h5>
-                 <input type="checkbox" name="5" value="público" style="margin-left: 20px"><h5>público</h5>
-                 <input type="checkbox" name="6" value="privado" style="margin-left: 20px"><h5>privado</h5>
-                 <input type="checkbox" name="7" value="social"  style="margin-left: 20px"><h5>social⁠⁠⁠⁠</h5>   
+                 <input type="checkbox" name="3" value="Financiadores">Financiadores<br>
+                 <input type="checkbox" name="4" value="Fortalecedor institucional">Fortalecedor institucional<br>
+                 <input type="checkbox" name="5" value="público" style="margin-left: 20px">público
+                 <input type="checkbox" name="6" value="privado" style="margin-left: 20px">privado
+                 <input type="checkbox" name="7" value="social"  style="margin-left: 20px">social⁠⁠⁠⁠   
                  </div>   
 
 
