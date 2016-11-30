efd157c836452bc711f6ae32f006943e914e3e2d
Willany Silva <willany_lp@hotmail.com>
2016-11-30 01:49:21 -0200

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -124,7 +124,7 @@ jQuery(document).ready(function() {
 
 
     //populando os combos de cidade e estado
-    $.getJSON('http://womanityecossistema.azurewebsites.net/index.php', function (data) {
+    //$.getJSON('http://womanityecossistema.azurewebsites.net/index.php', function (data) {
         var data = [{
       "sigla": "AC",
       "nome": "Acre",
@@ -5876,7 +5876,7 @@ jQuery(document).ready(function() {
             
         }).change();        
     
-    });
+    //});
 
     //form
     //$('#form-ong input').attr('readonly', 'readonly');
