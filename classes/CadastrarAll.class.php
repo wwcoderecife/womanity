64ddf59d178af4df213920c0eec6ee32148feb6b
wwcoderecife <karina@womenwhocode.com>
2016-12-08 12:30:35 -0300

    inserção da variável inputoutros

--- classes/CadastrarAll.class.php
+++ classes/CadastrarAll.class.php
@@ -701,7 +701,7 @@ class CadastrarAll extends Conexao {
             $inserir_ong = $pdo->prepare("insert into organizacoes (
                 cnpj, localizacao, nome, sigla, telefone, email, tipo, inicio_atv, qtde_pessoas, recursos_financeiros,
                 descricao,  publico_atendido, politicas_publicas, monitoramento_atividades, estrategia_comunicacao,
-                premiacao_certificacao, organizacao_pai, identifica_iniciativa, usuario_id, inputnomeong, inputoutrosprojetos, justificativa_orcamento, pessoas_benefeciadas)
+                premiacao_certificacao, organizacao_pai, identifica_iniciativa, usuario_id, inputnomeong, inputoutros, inputoutrosprojetos, justificativa_orcamento, pessoas_benefeciadas)
                                     values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,)");
             $inserir_ong->bindValue(1, $this->getCnpj());
             $inserir_ong->bindValue(2, $this->getLocalizacao());
