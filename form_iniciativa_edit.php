5a4d1a145822cf224d22ee7e009e69eebd4c8b2d
WWcode Recife <karina@womenwhocode.com>
2016-12-09 15:01:50 -0300

    edit do inputoutrosprojetos

--- form_iniciativa_edit.php
+++ form_iniciativa_edit.php
@@ -532,20 +532,15 @@
                                 if(in_array('venda de produtos/prestação de serviços', $array_recursos_financeiros[$i])) : ?> checked="checked" <?php endif; 
                         } ?>
                     >venda de produtos/prestação de serviços<br>
-                    <input type="checkbox" name="recursos_origem[]" value="outros"
+                     <input type="checkbox" name="recursos_origem[]" value="outros"
                         <?php 
                             for($i = 0; $i <= count($array_recursos_financeiros)-1; $i++){
                                 if(in_array('outros', $array_recursos_financeiros[$i])) : ?> checked="checked" <?php endif; 
                         } ?>
                     >outros<br>
 
-                    <input type="text" name="recursos_origem[]" placeholder="Descreva outras fontes de recursos..." value="inputoutrosprojetos"
-                        <?php 
-                            for($i = 0; $i <= count($array_recursos_financeiros)-1; $i++){
-                                if(in_array('inputoutrosprojetos', $array_recursos_financeiros[$i])) : ?> checked="checked" <?php endif; 
-                        } ?>
-                    ><br>
-                    <!-- <input type="text"     name="outrosprojetos" placeholder=" outros..." style="display:block;" class="f1-last-name form-control"> -->
+                    <input type="text" id="inputoutrosprojetos" name="inputoutrosprojetos" value="<?php echo $edit->getInputoutrosprojetos() ?>" class="f1-last-name form-control" <?php if($edit->getInputoutros() != ""){ echo "style='display: block'"; }else{echo "style='display: none'";} ?>    
+                    ><br>               
                </div> 
 
 
