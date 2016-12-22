1e78959806889c504edc5101fe85f75f8d513550
Willany Silva <willany_lp@hotmail.com>
2016-12-22 01:08:19 -0200

    Update form_iniciativa_edit.php

--- form_iniciativa_edit.php
+++ form_iniciativa_edit.php
@@ -406,18 +406,18 @@
                  <h5>Em 2014</h5>
                 <div class="form-group">
                     <label class="sr-only" for="f1-last-name">2014</label>
-                    <input type="text" name="orcamento_2014"  value="<?php  echo $edit->getOrcamento_2014(); ?>" placeholder="R$ xxxxxx" class="f1-last-name form-control money" id="iniciativa_orcamento2014">
+                    <input type="text" id="orcamento_2014" name="orcamento_2014"  value="<?php  echo $edit->getOrcamento_2014(); ?>" placeholder="R$ xxxxxx" class="f1-last-name form-control money" id="iniciativa_orcamento2014">
                     <div class="help-block with-errors"></div>
                 </div>
                 <h5>Em 2015</h5>
                 <div class="form-group">
                     <label class="sr-only" for="f1-last-name">2015</label>
-                    <input type="tex" name="orcamento_2015"   value="<?php  echo $edit->getOrcamento_2015(); ?>" placeholder="R$ xxxxxx" class="f1-last-name form-control money" id="iniciativa_orcamento2015">
+                    <input type="tex" id="orcamento_2015" name="orcamento_2015"   value="<?php  echo $edit->getOrcamento_2015(); ?>" placeholder="R$ xxxxxx" class="f1-last-name form-control money" id="iniciativa_orcamento2015">
                 </div>
                 <h5>Em 2016</h5>
                 <div class="form-group">
                     <label class="sr-only" for="f1-last-name">2016</label>
-                    <input type="tex" name="orcamento_2016"  value="<?php  echo $edit->getOrcamento_2016(); ?>" placeholder="R$ xxxxxx" class="f1-last-name form-control money" id="iniciativa_orcamento2016">
+                    <input type="tex" id="orcamento_2016" name="orcamento_2016"  value="<?php  echo $edit->getOrcamento_2016(); ?>" placeholder="R$ xxxxxx" class="f1-last-name form-control money" id="iniciativa_orcamento2016">
                     <div class="help-block with-errors"></div>
                 </div>
 
