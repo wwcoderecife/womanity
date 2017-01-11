40fba711ec13f3e778a5f5d3ef6f41ac4fa47aaa
WWcode Recife <karina@womenwhocode.com>
2017-01-11 17:51:01 -0200

    validatortel.js
    função de validação de tel, celular, cep, cnpj

---
+++ assets/js/validatortel.js
@@ -0,0 +1,20 @@
+<!-- Funçãoo limite caracteres tel,celular,cep, cnpj -->
+			function ExitField(id,idmsg,msg,maxSize){
+			
+			  var value = document.getElementById(id).value;
+			  			   			
+		 	   if(value.length != maxSize) {
+
+					  document.getElementById(id).focus();//use javascript to set focus
+					  document.getElementById(idmsg).innerHTML  = msg;
+					  if(isNaN(value)){
+						document.getElementById(idmsg).style.visibility = 'visible';
+					  }else{
+					    document.getElementById(idmsg).style.visibility = 'hidden';	  
+					  }
+				  
+				}else{
+					document.getElementById(idmsg).style.visibility = 'hidden';		
+				}
+					
+			}
