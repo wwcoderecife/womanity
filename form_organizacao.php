78bd00235407dd2ebc5d560f52abfd69197da377
wwcoderecife <karina@womenwhocode.com>
2017-01-16 18:40:52 -0300

    update

--- form_organizacao.php
+++ form_organizacao.php
@@ -1002,7 +1002,7 @@
                 <h5>Complemento</h5>
                 <div class="form-group">
                     <label class="sr-only" for="f1-google-plus">Complemento</label>
-                    <input type="text" name="complemento" placeholder="Complemento..." class="f1-last-name form-control"required>
+                    <input type="text" name="complemento" placeholder="Complemento..." class="f1-last-name form-control">
                 </div>
                  <h5>Cep</h5>
                 <div class="form-group">
