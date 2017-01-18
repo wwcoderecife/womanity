ed559f51976acade5a7956ca6a896a9ac2158c2d
wwcoderecife <karina@womenwhocode.com>
2017-01-18 13:58:55 -0300

    update

--- form_iniciativa_edit.php
+++ form_iniciativa_edit.php
@@ -235,7 +235,7 @@
                     <label class="radio-inline">
                         <input type="radio" name="zona" value="ambas" class="form-control-radio"
                          <?php if($edit->getLocalizacao() == "ambas"){ echo "checked='checked'"; } ?>
-                        >Zona Urbana
+                        >Ambas
                     </label>
                 </div>
 
