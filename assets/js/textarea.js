2d50b377f86d09541d6dbb7cd7fa8d648fc0f548
wwcoderecife <karina@womenwhocode.com>
2017-01-18 16:47:02 -0300

    função contador de caracteres textarea

---
+++ assets/js/textarea.js
@@ -0,0 +1,14 @@
+function caracterTextarea(box,num_max,campospan){
+ var contagem_carac = box.length;
+ if (contagem_carac != 0){
+ document.getElementById(campospan).innerHTML = contagem_carac + " caracteres digitados";
+ if (contagem_carac == 1){
+ document.getElementById(campospan).innerHTML = contagem_carac + " caracter digitado";
+ }
+ if (contagem_carac >= num_max){
+ document.getElementById(campospan).innerHTML = "Limite de caracteres excedido!";
+ }
+ }else{
+ document.getElementById(campospan).innerHTML = "campo com capacidade para 500 caracteres..";
+ }
+}
