a0ced5318f8cd64dda7b63ebbd56fd3f66f49c3c
wwcoderecife <karina@womenwhocode.com>
2017-02-10 22:34:01 -0300

    update

--- assets/js/campobranco.js
+++ assets/js/campobranco.js
@@ -9,7 +9,7 @@ return false
  
  
 
- if(document.getElementById("inputCargo1").value == ""){
+ else if(document.getElementById("inputCargo1").value == ""){
 
 	alert("Por favor preencha o campo Cargo");
 document.getElementById("inputCargo1").focus();
@@ -17,45 +17,45 @@ return false
 
 
 }
- if(document.getElementById("inputTel1").value == ""){
+ else if(document.getElementById("inputTel1").value == ""){
 	alert("Por favor preencha o campo Telefone");
 document.getElementById("inputTel1").focus();
 return false
 
 }
 
-if(document.getElementById("inputCel1").value == ""{
+else if(document.getElementById("inputCel1").value == ""{
 	alert("Por favor preencha o campo Celular");
 document.getElementById("inputCel1").focus();
 return false
 
 }
 
-if(document.getElementById("inputNome2").value == ""{
+else if(document.getElementById("inputNome2").value == ""{
 	alert("Por favor preencha o campo Nome");
 document.getElementById("inputNome2").focus();
 return false
 }
 
- if(document.getElementById("inputCargo2").value == ""{
+ else if(document.getElementById("inputCargo2").value == ""{
 	alert("Por favor preencha o campo Cargo");
 document.getElementById("inputCargo2").focus();
 return false
 }
 
- if(document.getElementById("inputEmail2").value == ""{
+ else if(document.getElementById("inputEmail2").value == ""{
 	alert("Por favor preencha o campo Email");
 document.getElementById("inputEmail2").focus();
 return false
 }
 
- if(document.getElementById("inputTel2").value == ""{
+ else if(document.getElementById("inputTel2").value == ""{
 	alert("Por favor preencha o campo Telefone");
 document.getElementById("inputCel1").focus();
 return false
 }
 
- if(document.getElementById("inputCel2").value == ""{
+ else if(document.getElementById("inputCel2").value == ""{
 	alert("Por favor preencha o campo Telefone");
 document.getElementById("inputCel2").focus();
 return false
