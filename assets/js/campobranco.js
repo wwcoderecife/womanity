5c0b24e30606a25fceb107ea14a6ba53f22cedfd
wwcoderecife <karina@womenwhocode.com>
2017-02-12 13:02:01 -0300

    up

--- assets/js/campobranco.js
+++ assets/js/campobranco.js
@@ -2,11 +2,11 @@
 function validateEmpty(fld) {
  var error = "";
   
-    if (fld.value.length === 0) {
-        fld.style.background = '#9370DB';
+    if (fld.value.length == 0) {
+        fld.style.background = 'Yellow'; 
         error = "Campo "+ fld.name +" obrigatório.\n"
     } else {
-        fld.style.background = '#FFFFFF';
+        fld.style.background = 'White';
     }
     return error;   
 }
