e53b29892498130b29fa1d9673a25450d5019e93
wwcoderecife <karina@womenwhocode.com>
2017-02-15 01:20:02 -0300

    update

--- form_organizacao.php
+++ form_organizacao.php
@@ -814,21 +814,22 @@
              <tbody>
 
                    <tr>
-                    <td><input type="checkbox" name="publico_alvo[ ]" value="Empresas" class="no-margin" onclick="verificar2()"></td>
-                    <td>Empresas.</td><br>
-                    <td class="publico_alvo" style="width:150px" onclick="verificar2()" >
-                        <input type="checkbox" name="publico_alvo[ ]" value="Organizações Sociais" >Organizações Sociais<br>
-                        <input type="checkbox" name="publico_alvo[ ]" value="Instituições públicas/órgãos públicos/legislativo/delegacia da mulher/secretaria da mulher">Instituições públicas/órgãos públicos/legislativo/delegacia da mulher/secretaria da mulher<br>
-                        <input type="checkbox" name="publico_alvo[ ]" value="Movimentos, coletivos, redes, fóruns">Movimentos, coletivos, redes, fóruns<br>
-                        <input type="checkbox" name="publico_alvo[ ]" value="Adolescentes e jovens">Adolescentes e jovens<br>
-                        <input type="checkbox" name="publico_alvo[ ]" value="Homens">Homens<br>
-                        <input type="checkbox" name="publico_alvo[ ]" value="Mulheres">Mulheres<br>
+                    <!--<td><input type="checkbox" name="publico_alvo[ ]" value="Empresas" class="no-margin" onclick="verificar2()"></td>-->
+                    <!--<td>Empresas.</td><br>-->
+                    <td class="publico_alvo" style="width:150px" >
+                        <input type="checkbox" name="publico_alvo[ ]" value="Empresas" onclick="verificar2()">Empresas<br>
+                        <input type="checkbox" name="publico_alvo[ ]" value="Organizações Sociais" onclick="verificar2()" >Organizações Sociais<br>
+                        <input type="checkbox" name="publico_alvo[ ]" value="Instituições públicas/órgãos públicos/legislativo/delegacia da mulher/secretaria da mulher" onclick="verificar2()">Instituições públicas/órgãos públicos/legislativo/delegacia da mulher/secretaria da mulher<br>
+                        <input type="checkbox" name="publico_alvo[ ]" value="Movimentos, coletivos, redes, fóruns" onclick="verificar2()">Movimentos, coletivos, redes, fóruns<br>
+                        <input type="checkbox" name="publico_alvo[ ]" value="Adolescentes e jovens" onclick="verificar2()">Adolescentes e jovens<br>
+                        <input type="checkbox" name="publico_alvo[ ]" value="Homens" onclick="verificar2()">Homens<br>
+                        <input type="checkbox" name="publico_alvo[ ]" value="Mulheres" onclick="verificar2()">Mulheres<br>
                     </td> 
 
             </tbody>        
 
                           <script>
-                            var CheckMaximo = 3;
+                            var CheckMAX = 3;
 
                             function verificar2() {
                             var Marcados = 1;
@@ -840,7 +841,7 @@
                                     Marcados++;
                                 }
                                 
-                                if (Marcados <= CheckMaximo) {
+                                if (Marcados <= CheckMAX) {
                                 //Habilitando todos os checkboxes, pois o máximo ainda não foi alcançado.
                                 for (var i=0; i<objCheck.length; i++) {
                                     objCheck[i].disabled = false;
