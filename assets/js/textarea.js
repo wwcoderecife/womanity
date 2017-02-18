023c0dfae208f3469b23a9540e307d2ca2fb706a
wwcoderecife <karina@womenwhocode.com>
2017-02-17 22:49:38 -0300

    update

--- assets/js/textarea.js
+++ assets/js/textarea.js
@@ -1,14 +1,14 @@
 function caracterTextarea(box,num_max,campospan){
  var contagem_carac = box.length;
  if (contagem_carac != 0){
- document.getElementById(campospan).innerHTML = contagem_carac + ".";
+ document.getElementById().innerHTML = contagem_carac + ".";
  if (contagem_carac == 1){
  document.getElementById(campospan).innerHTML = contagem_carac + ".";
  }
  if (contagem_carac >= num_max){
  document.getElementById(campospan).innerHTML = "Limite de caracteres excedido!";
  }
- }else{
- document.getElementById(campospan).innerHTML = ".";
- }
+ }//else{
+ //document.getElementById(campospan).innerHTML = ".";
+ //}
 }
