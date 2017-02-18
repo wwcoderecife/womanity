37e6d8bb2ef57b53ebeaf650d213ee51ecbe7739
wwcoderecife <karina@womenwhocode.com>
2017-02-18 08:43:17 -0300

    ajuste label Telefone iniciativa

--- form_iniciativa.php
+++ form_iniciativa.php
@@ -211,7 +211,7 @@
                 <h5>5. Informe o telefone da sua iniciativa*</h5>
                 <div class="form-group">
                     <label class="sr-only" >Telefone</label>
-                    <input type="text"  name="organizacao-telefone" onblur="ExitField('inputTel3','msgTel','Telefone inválido, número deve conter 14 caracteres ', 14);" required  placeholder="(xx) xxxx-xxxx"class="f1-last-name form-control phone_with_ddd" id="inputTel3">
+                    <input type="text"  name="organizacao-telefone" onblur="ExitField('inputTel3','msgTel3','Telefone inválido, número deve conter 14 caracteres ', 14);" required  placeholder="(xx) xxxx-xxxx"class="f1-last-name form-control phone_with_ddd" id="inputTel3">
                     <label id="msgTel3" ></label>
                 </div>
 
