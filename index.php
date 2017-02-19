370612053afd0501695a3f9cc6c132b387f6dcd6
wwcoderecife <karina@womenwhocode.com>
2017-02-19 14:47:59 -0300

    up

--- index.php
+++ index.php
@@ -202,7 +202,7 @@
 
                                 <div class="form-group">
                                     <label class="sr-only" for="f1-password">Senha *</label>
-                                    <input type="password" name="password" placeholder="Senha *" onkeypress=" return validasenha(event);" class="f1-password form-control" id="senha" maxlenght="6" required data-error = "Mínimo de seis (6) digitos">
+                                    <input type="password" name="password" placeholder="Senha *" onBlur=" return validasenha(event);" class="f1-password form-control" id="senha" maxlenght="6" required data-error = "Mínimo de seis (6) digitos">
                                     <span id="label" style="font-family:verdana;"></span>
                                     
                                 </div>
