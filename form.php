d5e531629e511b59dc5435bda3ece8f8db107be0
willany <willany_lp@hotmail.com>
2017-03-01 10:42:20 -0300

    Sessão do app

--- form.php
+++ form.php
@@ -62,6 +62,13 @@
 
     <body>
 <?php
+    
+    // server should keep session data for AT LEAST 1 hour
+    ini_set('session.gc_maxlifetime', 3600);
+
+    // each client should remember their session id for EXACTLY 1 hour
+    session_set_cookie_params(3600);
+
     session_start();
 
     require_once "classes/Conexao.class.php";
