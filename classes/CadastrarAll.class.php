92232a72bffaf8ce7386de1e8ded029366949fcf
wwcoderecife <karina@womenwhocode.com>
2017-03-19 20:56:32 -0300

    comentando função enviar e-mail cadastra.all
    Para o envio de e-mail de aplicações hospedadas no azure, suporta o
    sendgride, irei configurar o sendgride no portal do azure.

--- classes/CadastrarAll.class.php
+++ classes/CadastrarAll.class.php
@@ -1157,7 +1157,7 @@ class CadastrarAll extends Conexao {
                 $_SESSION['cadastro'] = true;
 
                    
-  
+               /* 
                $email_1 = $this->getEmail_1();
                $nome = $this->getNome_1();
 
@@ -1178,7 +1178,7 @@ class CadastrarAll extends Conexao {
                     // metodo enviar email
                     smtpmailer($email_1, "machado.karina@gmail.com", "Karina Machado", $assuntocompleto,$corpo);
 
-
+*/
 
                     echo "<script type='text/javascript'>
 
@@ -1608,28 +1608,7 @@ class CadastrarAll extends Conexao {
 
             $_SESSION['cadastro'] = true;
 
-             $email_1 = $this->getEmail_1();
-               $nome = $this->getNome_1();
-
-               $corpo = "Prezado(a) $nome_1
-                            \n\n
-                            Todas as informações foram registradas em nosso sistema.
-                            \n
-                            Valorizamos muito a sua contribuição para o Mapeamento do Ecossistema de Iniciativas para equidade de gênero e empoderamento da mulher. 
-                            \n
-                            Muito Obrigada
-                            \n
-                            Nos próximos meses você poderá conhecer as outras organizações e iniciativas cadastradas e indicar com quais mais tem se relacionado. 
-                            \n
-                            Até breve!
-
-                            Equipe Executiva";
-                    $assuntocompleto = "Cadastro Ecossistema " .$nome;
-                    // metodo enviar email
-                    smtpmailer($email_1, "machado.karina@gmail.com", "Karina Machado", $assuntocompleto,$corpo);
-
-
-            
+                      
             echo "<script type='text/javascript'>
 
                         sweetAlert({
