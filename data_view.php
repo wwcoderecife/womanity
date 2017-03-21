125ac86041a94ef755348e58c45103b447be8897
Willany Silva <willany_lp@hotmail.com>
2017-03-21 00:44:58 -0300

    Update data_view.php

--- data_view.php
+++ data_view.php
@@ -1,6 +1,15 @@
-<html>
-  <head>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ecossistema</title>
+    <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
 	<?php
 
