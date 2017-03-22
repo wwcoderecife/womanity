03e3a23ccba251cd0d43d0c0399a6bb64804d711
wwcoderecife <karinamachado@msn.com>
2017-03-22 13:20:23 -0300

    retirando função enviar email do git do wwcode.

--- classes/CadastrarAll.class.php
+++ classes/CadastrarAll.class.php
@@ -43,7 +43,7 @@
 <?php
 
 include_once 'Conexao.class.php';
-include("sendmail.php");
+
 
 
 class CadastrarAll extends Conexao {
@@ -1608,31 +1608,7 @@ class CadastrarAll extends Conexao {
 
             $_SESSION['cadastro'] = true;
 
-              // $email_1 = $this->getEmail_1();
-              // $nome = $this->getNome_1();
-           
-              $email_1 = 'maikryuge@gmail.com';
-               $nome = 'Maik Ryuge';
-
-
-               $corpo = "Prezado(a) $nome
-                            \n\n
-                            Todas as informações foram registradas em nosso sistema.
-                            \n
-                            Valorizamos muito a sua contribuição para o Mapeamento do Ecossistema de Iniciativas para equidade de gênero e empoderamento da mulher. 
-                            \n
-                            Muito Obrigada
-                            \n
-                            Nos próximos meses você poderá conhecer as outras organizações e iniciativas cadastradas e indicar com quais mais tem se relacionado. 
-                            \n
-                            Até breve!
-
-                            Equipe Executiva";
-                    $assuntocompleto = "Cadastro Ecossistema " .$nome;
-                    // metodo enviar email
-                    smtpmailer($email_1, "machado.karina@gmail.com", "Karina Machado", $assuntocompleto,$corpo);
-
-           /*          
+                    
             echo "<script type='text/javascript'>
 
                         sweetAlert({
@@ -1645,7 +1621,7 @@ class CadastrarAll extends Conexao {
                         });
 
                     </script>";
-                 */   
+                   
 
         }
 
